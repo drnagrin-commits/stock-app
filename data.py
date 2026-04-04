84e477dcd96ff711bd996747d9df80836c1f5d40
drnagrin-commits <drnagrin@gmail.com>
2026-04-04 16:17:56 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,8 @@
 def get_nasdaq100():
     return [
-        "AAPL","MSFT","NVDA"
+        "AAPL","MSFT","NVDA","AMZN","META","GOOGL","GOOG","TSLA",
+        "AVGO","COST","PEP","ADBE","CSCO","NFLX","AMD","INTC",
+        "CMCSA","TXN","QCOM","AMGN","HON","INTU","ISRG","AMAT",
+        "BKNG","ADI","MDLZ","LRCX","MU","GILD","PANW","SNPS",
+        "CDNS","KLAC","VRTX","REGN","ADP","MAR","MNST","FTNT"
     ]
